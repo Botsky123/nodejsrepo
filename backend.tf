0cc2f700daf53230e0c16e9da1489eaca46eb6ac
Botsky123 <104427127+Botsky123@users.noreply.github.com>
2024-02-06 17:15:18 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,8 +1,1 @@
-terraform {
-  backend "azurerm" {
-    resource_group_name  = "Azure-akhil07"
-    storage_account_name = "akhil07"
-    container_name       = "tfstate07"
-    key                  = "terraform.tfstate"
-  }
-}
+
